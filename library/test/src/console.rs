ba3b90dfd3db422cf90033633ded90fba6cde584
Erik Kaneda <erik@risczero.com>
2023-11-15 16:57:32 -0800

    rustc: implement support for `riscv32im_risc0_zkvm_elf`
    This also adds changes in the rust test suite in order to get a few of them to
    pass.
    
    Co-authored-by: Frank Laub <flaub@risc0.com>
    Co-authored-by: Urgau <3616612+Urgau@users.noreply.github.com>

--- library/test/src/console.rs
+++ library/test/src/console.rs
@@ -315,8 +315,10 @@ pub fn run_tests_console(opts: &TestOpts, tests: Vec<TestDescAndFn>) -> io::Resu
 
     // Prevent the usage of `Instant` in some cases:
     // - It's currently not supported for wasm targets.
-    let is_instant_unsupported =
-        (cfg!(target_family = "wasm") && !cfg!(target_os = "wasi")) || cfg!(target_os = "zkvm");
+    // - We disable it for miri because it's not available when isolation is enabled.
+    let is_instant_unsupported = (cfg!(target_family = "wasm") && !cfg!(target_os = "wasi"))
+        || cfg!(target_os = "zkvm")
+        || cfg!(miri);
 
     let start_time = (!is_instant_unsupported).then(Instant::now);
     run_tests(opts, tests, |x| on_test_event(&x, &mut st, &mut *out))?;
